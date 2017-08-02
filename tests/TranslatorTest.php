6a9d51900ade0d7c1ca78c68d265d87bce216cee
kilych <kilych@zoho.com>
2017-08-03 04:40:22 +0700

    Extend TranslatorTest

--- tests/TranslatorTest.php
+++ tests/TranslatorTest.php
@@ -4,6 +4,7 @@ namespace Wikitran;
 
 use PHPUnit\Framework\TestCase;
 use Wikitran\Translator;
+use Wikitran\Core\Migration;
 
 class TranslatorTest extends TestCase
 {
@@ -14,9 +15,25 @@ class TranslatorTest extends TestCase
     {
         $translated = 'How the Steel Was Tempered';
         $tr = new Translator();
-        $tr->setMethod('web');
 
-        $this->assertEquals('web', $tr->getMethod());
+        $this->assertEquals('mixed', $tr->getMethod());
+        $this->assertEquals($translated, $tr->translate('ru', 'en', 'как закалялась сталь'));
+
+        $tr->setMethod('db');
+        $this->assertEquals('db', $tr->getMethod());
+        $this->assertEquals($translated, $tr->translate('ru', 'en', 'как закалялась сталь'));
+
+        $dbpath = Migration::createDbFile(__DIR__ . '/data');
+        $pdo = Migration::connectSQLite($dbpath);
+        Migration::clear($pdo);
+        Migration::run($pdo);
+        $tr = new Translator($pdo);
+
+        $this->assertEquals('mixed', $tr->getMethod());
+        $this->assertEquals($translated, $tr->translate('ru', 'en', 'как закалялась сталь'));
+
+        $tr->setMethod('db');
+        $this->assertEquals('db', $tr->getMethod());
         $this->assertEquals($translated, $tr->translate('ru', 'en', 'как закалялась сталь'));
     }
 }
