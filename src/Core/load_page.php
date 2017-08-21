d1ed07098afe9aeca5879f5223305f8baa115f08
kilych <kilych@zoho.com>
2017-08-21 13:48:04 +0700

    Improve function Core\load_page

--- src/Core/load_page.php
+++ src/Core/load_page.php
@@ -2,7 +2,7 @@
 
 namespace Wikitran\Core;
 
-function load_page(string $lang, array $queries)
+function load_page(string $source, array $queries)
 {
     if (extension_loaded('curl')) {
         // create curl resource
@@ -12,10 +12,12 @@ function load_page(string $lang, array $queries)
 
         foreach ($queries as $try) {
             // set url
-            curl_setopt($ch, CURLOPT_URL, url_from_query($lang, $try));
+            $url = url_from_query($source, $try);
+            curl_setopt($ch, CURLOPT_URL, $url);
             $page = curl_exec($ch);
-            if (!curl_errno($ch)
-                && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200) {
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            // error_log(__METHOD__ . " $url : $httpCode");
+            if (! curl_errno($ch) && $httpCode === 200) {
                 return [$try, $page];
             }
         }
@@ -25,7 +27,7 @@ function load_page(string $lang, array $queries)
     } else {
         foreach ($queries as $try) {
             // stfu for 404 warnings
-            $page = @file_get_contents(url_from_query($lang, $try));
+            $page = @file_get_contents(url_from_query($source, $try));
             if ($page) {
                 return [$try, $page];
             }
@@ -34,11 +36,11 @@ function load_page(string $lang, array $queries)
     return false;
 }
 
-function url_from_query(string $lang, string $query)
+function url_from_query(string $source, string $query)
 {
     $term = str_replace(' ', '_', $query);
-    if ($lang !== 'en') {
+    if ($source !== 'en') {
         $term = urlencode($term);
     }
-    return "https://$lang.wikipedia.org/wiki/$term";
+    return "https://$source.wikipedia.org/wiki/$term";
 }
